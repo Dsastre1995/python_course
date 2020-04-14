c7820a3d877700783ef75926c2660f0277e7f696
David Sastre <developer.sastre1995@gmail.com>
2020-04-14 22:46:36 +0200

    add katas folder

---
+++ katas/katas.py
@@ -0,0 +1,9 @@
+def dig_pow(n, p):
+    splited_nums = list((int(num) for num in str(n)))
+    powered_nums = list(num for num in range(p, p + len(splited_nums) + 1))
+    result = sum(num ** powered_num for num, powered_num in zip(splited_nums, powered_nums))
+    num_result = list(filter(lambda num: n * num == result, range(1, 1000001)))
+
+    return num_result[0] if len(num_result) > 0 else -1
+
+print(dig_pow(695, 2))
