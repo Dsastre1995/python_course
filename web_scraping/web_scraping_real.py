77f1a98ac8d9181a1ebf808c25d2ddd4bd6706e9
David Sastre <developer.sastre1995@gmail.com>
2020-04-29 09:10:46 +0200

    Create real web scraping project file in order to practise with a real case

---
+++ web_scraping/web_scraping_real.py
@@ -0,0 +1,22 @@
+# https://www.rithmschool.com/blog
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+
+response = requests.get('https://www.rithmschool.com/blog')
+soup = BeautifulSoup(response.text, 'html.parser')
+articles = soup.find_all('article')
+    
+with open('blog_data.csv', 'w') as file:
+    csv_writer = writer(file)
+    csv_writer.writerow(['title', 'link', 'date'])
+
+    for article in articles:
+        a_tag = article.find('a')
+        time_tag = article.find('time')
+
+        title = a_tag.get_text()
+        link = a_tag['href']
+        date = time_tag['datetime']
+
+        csv_writer.writerow([title, link, date])
